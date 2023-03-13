3379a78f7e5c412fabf52991102d5f54f6bf5ce5
jude-shreffler <js467820@ohio.edu>
2023-03-13 14:04:30 -0400

    :lipstick::sparkles: Created a main page and a contact page that link through react router

---
+++ frontend/app/src/Contact.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+import "./App.css";
+
+function Contact() {
+  
+    return (
+        <>
+            <p>Contact Page</p>
+            <Link to={'/'}>Home Page</Link>
+        </>
+    );
+}
+
+export default Contact;
--- frontend/app/src/main.jsx
+++ frontend/app/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Contact from './Contact'
 import './index.css'
+
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/contact",
+    element: <Contact />,
+  }
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

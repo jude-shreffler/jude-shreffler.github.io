4ab94139b0f9b0d556ea5194549ea9c157e9326e
jude-shreffler <js467820@ohio.edu>
2023-03-13 15:07:53 -0400

    :lipstick: contact form css pretty presentable!

--- frontend/app/src/Contact.jsx
+++ frontend/app/src/Contact.jsx
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom"
-import "./App.css";
+import "./Contact.css";
 
 function Contact() {
   
     return (
         <>
-            <p>Contact Page</p>
+            <p>Want to get in touch?</p>
+            <div class="card">
+                <form>
+                    <label for="name">Name:</label><br />
+                    <input type="text" id="name" name="name" /><br />
+                    <label for="email">Email:</label><br />
+                    <input type="text" id="email" name="email" /><br />
+                    <label for="message">Message:</label><br />
+                    <textarea type="text" id="message" name="message" rows="2"/><br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
             <Link to={'/'}>Home Page</Link>
         </>
     );

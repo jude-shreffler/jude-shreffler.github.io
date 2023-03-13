8dd356d3b77f3f11eac74d70c8b47f33d3e7172c
jude-shreffler <js467820@ohio.edu>
2023-03-13 12:02:52 -0400

    :tada: setup complete from tailwind's instructions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jude-shreffler.github.io
+
+Website for and by Jude Shreffler, made in the software development club web dev seminar.
---
+++ frontend/app/tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
